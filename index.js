705899262c276ac871ed7f59713d4a42e775d610
iqratariq123 <165536758+iqratariq123@users.noreply.github.com>
2024-04-01 09:30:11 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+console.log("By iqra tariq");
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter firstNumber", type: "number", name: "firstNumber" },
+    { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+    { message: "select one of the operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+console.log(answer);
+//CONDITIONAL STATEMENT
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("please select valid operator");
+}
---
+++ index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+console.log("By iqra tariq")
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+  { message: "Enter your firstNumber" , type: "number", name: "firstNumber"},
+  {message: "Enter your secondNumber" , type: "number", name: "secondNumber"},
+  {message: "select one of the operator to perform operation",
+   type: "list", 
+   name: "operator",
+choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+},
+]);
+console.log(answer)
+
+//CONDITIONAL STATEMENT
+
+ if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if(answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if(answer.operator === "Multiplication") {
+  console.log(answer.firstNumber * answer.secondNumber)
+} else if(answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("please select valid operator");
+}
+console.log("THE END")
